b626c681e5ceb62480a6ddb3674cde3145b9d46c
Oscar David Arce <oscar.arce@almundo.com>
2020-07-02 20:00:30 -0500

    Updates

--- precache-manifest.b6c12d09e35049b3d9133df17bc8aef8.js
+++ precache-manifest.b6c12d09e35049b3d9133df17bc8aef8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "edad9905856ccc109bd9d4f8b8d0d825",
+    "revision": "4c65b2566ec4e60ca1d5f6ab04ba39bb",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "ecb52680b65022a37c64",
+    "revision": "cc35caa2a1ba74a14fce",
     "url": "/portfolio/static/css/main.58598703.chunk.css"
   },
   {
-    "revision": "211049cb878bd0da9d96",
-    "url": "/portfolio/static/js/2.30a5e173.chunk.js"
+    "revision": "fbad22b31128eda35c06",
+    "url": "/portfolio/static/js/2.83d958b2.chunk.js"
   },
   {
     "revision": "c87550333da8df8ab4c80bd1d6e9defb",
-    "url": "/portfolio/static/js/2.30a5e173.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.83d958b2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ecb52680b65022a37c64",
-    "url": "/portfolio/static/js/main.86d71bb9.chunk.js"
+    "revision": "cc35caa2a1ba74a14fce",
+    "url": "/portfolio/static/js/main.2c510209.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
@@ -30,5 +30,13 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c9bd708a7e45a69da79480e18b258ddf",
     "url": "/portfolio/static/media/homeLogo.c9bd708a.png"
+  },
+  {
+    "revision": "e2f3f438dbc2239955b1755bba96c918",
+    "url": "/portfolio/static/media/logo-almundo.e2f3f438.svg"
+  },
+  {
+    "revision": "08f5f245ab738ec838ad123b3b70a3d7",
+    "url": "/portfolio/static/media/logo-hbt.08f5f245.png"
   }
 ]);
